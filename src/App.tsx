8f87418ab099f6c858a5f37e01b28ee910c582c5
david.barker@johnlewis.co.uk <WWjzHp7mtnBcmx42xvev>
2020-10-08 17:43:06 +0100

    Added styled system example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from './components/Button';
+import { Button, StyledSystemButton } from './components/Button';
 
 export const App: React.FC = () => {
   const [hasBorder, setHasBorder] = React.useState(false);
@@ -10,6 +10,10 @@ export const App: React.FC = () => {
     <>
       <Button border={hasBorder}>My border toggles</Button>
       <Button onClick={onToggleBorder}>Toggle border on the first button</Button>
+
+      <br />
+
+      <StyledSystemButton border="1px solid red">Styled system button</StyledSystemButton>
     </>
   );
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,17 @@
 import styled from '@emotion/styled';
+import { border, BorderProps, flexbox } from 'styled-system';
 
 export type ButtonProps = {
-    border: boolean;
-}
+  border: boolean;
+};
 
+export type StyledSystemButtonProps = BorderProps;
+
+// Super simple button
 export const Button = styled('button')<ButtonProps>(({ border = false }) => ({
-    borderStyle: 'solid',
-    borderColor: border ? 'red': 'transparent',
-    borderWidth: 1,
+  borderStyle: 'solid',
+  borderColor: border ? 'red' : 'transparent',
+  borderWidth: 1,
 }));
+
+export const StyledSystemButton = styled('button')<StyledSystemButtonProps>(border);
